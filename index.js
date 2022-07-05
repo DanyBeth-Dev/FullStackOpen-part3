47607ed3efce86653c10097e6e9dc478a297e5b3
Daniela Hernández <105611396+DanyBeth-Dev@users.noreply.github.com>
2022-07-05 17:21:38 -0400

    added requestLogger

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ var morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(requestLogger)
 app.use(express.static('build'))
 
 let persons = [
